ab1dcd55c046e5738322d8c9b783bd342ff57169
Hidetake Iwata <int128@gmail.com>
2023-04-30 11:49:39 +0900

    Show pulls

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+import { GitHub } from '@actions/github/lib/utils'
 import { App } from '@octokit/app'
+
+type Octokit = InstanceType<typeof GitHub>
 
 type Inputs = {
   appId: string
@@ -12,6 +14,22 @@ export const run = async (inputs: Inputs): Promise<void> => {
   const app = new App({
     appId: inputs.appId,
     privateKey: inputs.appPrivateKey,
+    Octokit: GitHub,
   })
-  await app.eachRepository((r) => core.info(`repo ${r.repository.full_name}`))
+  const { data: appAuthenticated } = await app.octokit.rest.apps.getAuthenticated({})
+  core.info(`authenticated as ${appAuthenticated.name}`)
+  for await (const { octokit, repository } of app.eachRepository.iterator()) {
+    await processRepository(octokit, repository.owner.login, repository.name)
+  }
+}
+
+const processRepository = async (octokit: Octokit, owner: string, repo: string) => {
+  const { data: pulls } = await octokit.rest.pulls.list({
+    owner,
+    repo,
+    state: 'open',
+  })
+  for (const pull of pulls) {
+    core.info(`${owner}/${repo}#${pull.number} ${pull.user?.login ?? ''}`)
+  }
 }
