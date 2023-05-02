5ca4b1c1abdcb62e8cd01d436048daa369f153bb
Hidetake Iwata <int128@gmail.com>
2023-05-02 11:42:36 +0900

    Refactor (#11)
    * Refactor
    
    * Fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const main = async (): Promise<void> => {
   await run({
     appId: core.getInput('github-app-id', { required: true }),
     appPrivateKey: core.getInput('github-app-private-key', { required: true }),
+    dryRun: core.getBooleanInput('dry-run'),
   })
 }
 
--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import { GitHub } from '@actions/github/lib/utils'
 import { App } from '@octokit/app'
-import { queryPulls } from './queries/pulls'
+import { fetchPulls } from './queries/pulls'
 import { determinePullRequestAction, parsePayload } from './pulls'
 
 type Octokit = InstanceType<typeof GitHub>
@@ -9,6 +9,7 @@ type Octokit = InstanceType<typeof GitHub>
 type Inputs = {
   appId: string
   appPrivateKey: string
+  dryRun: boolean
 }
 
 export const run = async (inputs: Inputs): Promise<void> => {
@@ -17,22 +18,27 @@ export const run = async (inputs: Inputs): Promise<void> => {
     privateKey: inputs.appPrivateKey,
     Octokit: GitHub,
   })
-  const { data: appAuthenticated } = await app.octokit.rest.apps.getAuthenticated({})
-  core.info(`authenticated as ${appAuthenticated.name}`)
+  const { data: appAuthenticated } = await app.octokit.rest.apps.getAuthenticated()
+  core.info(`Authenticated as ${appAuthenticated.name}`)
   for await (const { octokit, repository } of app.eachRepository.iterator()) {
-    await processRepository(octokit, repository.owner.login, repository.name)
+    await processRepository(octokit, repository.owner.login, repository.name, inputs.dryRun)
   }
 }
 
-const processRepository = async (octokit: Octokit, owner: string, repo: string) => {
-  const rawPulls = await queryPulls(octokit, { owner, repo })
-  core.startGroup(`Pull Requests in ${owner}/${repo}`)
-  core.info(JSON.stringify(rawPulls, undefined, 2))
+const processRepository = async (octokit: Octokit, owner: string, repo: string, dryRun: boolean) => {
+  const pullsQuery = await fetchPulls(octokit, { owner, repo })
+  core.startGroup(`Repository ${owner}/${repo}`)
+  core.info(JSON.stringify(pullsQuery, undefined, 2))
   core.endGroup()
-  const pulls = parsePayload(rawPulls)
+
+  const pulls = parsePayload(pullsQuery)
   for (const pull of pulls) {
     const action = determinePullRequestAction(pull)
-    core.info(`${pull.owner}/${pull.repo}#${pull.number}: ${String(action)}`)
-    await action.execute(octokit, pull)
+    core.info(`Pull Request ${owner}/${repo}#${pull.number}: ${action.toString()}`)
+    if (dryRun) {
+      core.info(`(dry-run)`)
+    } else {
+      await action.execute(octokit, pull)
+    }
   }
 }
