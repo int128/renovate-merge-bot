c438417f1a6c987296ac7f37ca8de846602b694b
Hidetake Iwata <int128@gmail.com>
2025-03-15 13:59:33 +0900

    Refactor index.ts (#655)
    * Refactor index.ts
    
    * Fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import * as core from '@actions/core'
 import { run } from './run.js'
 
-const main = async (): Promise<void> => {
+try {
   await run({
     appId: core.getInput('github-app-id', { required: true }),
     appPrivateKey: core.getInput('github-app-private-key', { required: true }),
     dryRun: core.getBooleanInput('dry-run'),
   })
-}
-
-main().catch((e: Error) => {
-  core.setFailed(e)
+} catch (e) {
+  core.setFailed(e instanceof Error ? e : String(e))
   console.error(e)
-})
+}
